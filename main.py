0a65c36a99f10092bd129240ddf60f032085225c
Adams LaBorde <juvenfly@gmail.com>
2017-10-26 16:21:40 -0500

    add get_leaf_value helper

--- main.py
+++ main.py
@@ -35,6 +35,12 @@ def main():
 
 
 def collect_issues(jirapi_conn, data_frame):
+    """
+    Compile all issues from a given board into
+    :param jirapi_conn:
+    :param data_frame:
+    :return:
+    """
     for issue in jirapi_conn.all_issues():
         row_index = get_issue_key(issue)
         row_dict = parse_issue_json(issue)
@@ -45,24 +51,25 @@ def collect_issues(jirapi_conn, data_frame):
 
 
 def parse_issue_json(issue):
-    row_dict = {
-        'key': issue['key'],
-        'summary': issue['fields']['summary'],
-        'issue_type': issue['fields']['issuetype']['name'],
-        'components': ','.join([issue['fields']['components'][i]['name'] for i, obj in enumerate(issue['fields']['components'])]),
-        'fix_versions': ','.join([issue['fields']['fixVersions'][i]['name'] for i, obj in enumerate(issue['fields']['fixVersions'])]),
-        'reporter': issue['fields']['reporter']['displayName'],
-        'assignee': issue['fields']['assignee']['displayName'],
-        'created_datetime': issue['fields']['created'],
-        'updated_datetime': issue['fields']['updated'],
-        'resolved_datetime': issue['fields']['resolutiondate'],
-        'status': issue['fields']['status']['name'],
-        'labels': ','.join(issue['fields']['labels']),
-        'original_estimate': issue['fields']['timetracking'].get('originalEstimateSeconds'),
-        'remaining_estimate': issue['fields']['timetracking'].get('remainingEstimateSeconds'),
-        'time_spent': issue['fields']['timetracking'].get('timeSpentSeconds'),
-    }
+    row_dict = {key: get_leaf_value(issue, HEADER) for key in HEADER}
     return row_dict
+
+
+def get_leaf_value(issue_json, keys):
+    """
+    Descends JSON object based on list of keys and returns leaf value.
+    :param issue_json: issue_json from JIRA API
+    :param keys: list of keys in order from outermost to innermost
+    :return: leaf value or None if there is a None-ish value anywhere along the way
+    """
+    result = issue_json
+    for i, key in enumerate(keys):
+        result = result.get(keys[i])
+        if result is None:
+            break
+    if isinstance(result, list):
+        result = ','.join(result)
+    return result
 
 
 def get_issue_key(issue):
