8740924d40f3c4ea05710e36a83d8d7a101e6b33
Adams LaBorde <juvenfly@gmail.com>
2017-10-30 15:24:29 -0500

    rename generate_time_estimates_plot to time_estimates_plot

--- plotter.py
+++ plotter.py
@@ -2,7 +2,7 @@ from plotly import plotly
 from plotly.graph_objs import Scatter, Bar, Layout, Data, Figure
 
 
-def generate_time_estimates_plot(data_frame, xrange=None):
+def time_estimates_plot(data_frame, xrange=None):
     trace0 = Scatter(
         x=data_frame.index.tolist(),
         y=data_frame['time_spent'],
