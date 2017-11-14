e5c771ddf4c9088088856bd9ea29343d7e3ac6cd
Adams LaBorde <juvenfly@gmail.com>
2017-11-14 16:07:22 -0600

    refactor ProductionSupportFilters

--- jql.py
+++ jql.py
@@ -2,7 +2,6 @@
 
 class ProductionSupportFilters(object):
 
-    def __init__(self):
-        self.all_issues = "project in (FARM) AND (cf[10614] = 18 OR labels in (totalpkg, prodsup)) ORDER BY Rank ASC"
-        self.high_priority_issues = "project in (FARM) AND (cf[10614] = 18 OR labels in (totalpkg, prodsup)) AND (Flagged is not EMPTY OR priority = Blocker) AND sprint in (openSprints(), futureSprints()) ORDER BY Rank ASC"
-        self.open_sprints = "project in (FARM, SB, DI, DE) AND (cf[10614] = 18 OR labels in (totalpkg, prodsup)) AND sprint in (openSprints(), futureSprints()) ORDER BY Rank ASC"
+    all_issues = "project in (FARM) AND (cf[10614] = 18 OR labels in (totalpkg, prodsup)) ORDER BY Rank ASC"
+    high_priority_issues = "project in (FARM) AND (cf[10614] = 18 OR labels in (totalpkg, prodsup)) AND (Flagged is not EMPTY OR priority = Blocker) AND sprint in (openSprints(), futureSprints()) ORDER BY Rank ASC"
+    open_sprints = "project in (FARM, SB, DI, DE) AND (cf[10614] = 18 OR labels in (totalpkg, prodsup)) AND sprint in (openSprints(), futureSprints()) ORDER BY Rank ASC"
