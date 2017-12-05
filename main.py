0d06e784aaaa36bd4609cf61c5208a2f12918552
Adams LaBorde <juvenfly@gmail.com>
2017-12-05 11:34:35 -0600

    tfidf vectorize text fields

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from argparse import ArgumentParser
 
+import numpy
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
 
 from api import JirApi
 from constants import HEADER, FIELD_MAP
@@ -36,6 +38,8 @@ def main():
         # TODO: validate header from archive is same as above
         data_frame = pd.DataFrame.from_csv('issues.csv')
     except FileNotFoundError:
+        if not update_issues:
+            raise
         data_frame = pd.DataFrame(columns=HEADER)
 
     if update_issues:
@@ -43,6 +47,22 @@ def main():
         data_frame = jira.collect_issues(data_frame)
         data_frame.to_csv('issues.csv')
 
+    data_frame = vectorize_text_fields(data_frame)
+
+    x_vals = data_frame.drop('time_spent', axis=1)
+    y_vals = data_frame['time_spent']
+
+    x_train, x_test, y_train, y_test = train_test_split(x_vals, y_vals, test_size=0.3, random_state=100)
+
+
+def vectorize_text_fields(data_frame):
+    vectorizer = TfidfVectorizer()
+    for column_name in data_frame:
+        if data_frame[column_name].dtype == numpy.object:
+            tfidf_vect = vectorizer.fit_transform(data_frame[column_name])
+            vect_column_name = column_name + '_tfidf'
+            data_frame[vect_column_name] = tfidf_vect
+    return data_frame
 
 if __name__ == '__main__':
     main()
